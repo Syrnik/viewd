6736413703d4a5b25a6a75f8b53313a2c861b034
Serge Rodovnichenko <sergerod@gmail.com>
2019-09-17 21:00:11 +0300

    Добавил манифест с системными требованиями

---
+++ lib/config/requirements.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @copyright (c) 2015-2019, Serge Rodovnichenko
+ * @license http://www.webasyst.com/terms/#eula Webasyst
+ */
+
+return array(
+    'php' => ['version' => '>=5.6.0', 'strict' => true],
+);
