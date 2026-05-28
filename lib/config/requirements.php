5c26d479fad88b825d68543706bc7a84e7c27406
Serge Rodovnichenko <sergerod@gmail.com>
2026-05-28 10:44:39 +0300

    chore: Raised minimal required PHP version up to 7.4 #29.6

--- lib/config/requirements.php
+++ lib/config/requirements.php
@@ -1,10 +1,11 @@
 <?php
 /**
  * @author Serge Rodovnichenko <serge@syrnik.com>
- * @copyright (c) 2015-2019, Serge Rodovnichenko
+ * @copyright (c) 2015-2026, Serge Rodovnichenko
  * @license http://www.webasyst.com/terms/#eula Webasyst
  */
 
-return array(
-    'php' => ['version' => '>=7.2.0', 'strict' => true],
-);
+return [
+    'php' => ['version' => '>=7.4.0', 'strict' => true],
+    'app.installer' => ['version' => '>=3.0.0', 'strict' => true],
+];
